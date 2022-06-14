cd038be1399f3e3a7ae3b260e817f00a40ee322f
aungkyawlin <aungkyawlinmon@gmail.com>
2022-06-14 22:09:54 +0700

    Login page complete

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 // login_page.dart
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:my_stock/src/config/theme.dart' as custom_theme;
 import 'package:my_stock/src/pages/login/widgets/header.dart';
@@ -21,18 +23,41 @@ class LoginPage extends StatelessWidget {
               gradient: custom_theme.Theme.gradient,
             ),
           ),
-          Column(
-            children: [
-              Header(), // เรียกใช Widget จาก lib/src/pages/login/widgets/header.dart
-              LoginForm(), // เรียกใช Widget จาก lib/src/pages/login/widgets/login_form.dart
-              Text('forgot password', style: TextStyle(color: Colors.white)),
-              SingleSignOn(), // เรียกใช Widget จาก lib/src/pages/login/widgets/single_sign_on.dart
-              Text('register', style: TextStyle(color: Colors.white)),
-            ],
+          // แก่ปัญหา Overflow ด้วย SingleChildScrollView
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Header(),
+                LoginForm(),
+                SizedBox(height: 30),
+                _buildFlatButton('Forgot Password', onPressed: () {}),
+                SingleSignOn(),
+                SizedBox(height: 30),
+                _buildFlatButton("Don't have an Account", onPressed: () {}),
+                SizedBox(height: 80),
+                // เพื่อให้มีช่องว่างด้านล่าง
+              ],
+            ),
           ),
         ],
       ),
     );
   }
-}
 
+  FlatButton _buildFlatButton(
+    String text, {
+    required VoidCallback onPressed,
+    double fontSize = 16.0,
+  }) =>
+      FlatButton(
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: fontSize,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      );
+}
